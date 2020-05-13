7d96dc327b2795a58667ad3ac4a83d813e5a7e10
Vizkrig <mail.vishnupk@gmail.com>
2020-05-13 19:58:42 +0530

    Add react app for location tracking

---
+++ precache-manifest.5d61a5a3f174f87fd2b1da0fb6cd3bb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cca9e69ac1df131b8caf9b3efca2ff5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02d0335eae42e6820fd8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b6e66f9424220b91f750",
+    "url": "/static/js/2.2fe17a97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2fe17a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02d0335eae42e6820fd8",
+    "url": "/static/js/main.18a9d4f3.chunk.js"
+  },
+  {
+    "revision": "391ed2b998926c7feaa2",
+    "url": "/static/js/runtime-main.8393f8aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
